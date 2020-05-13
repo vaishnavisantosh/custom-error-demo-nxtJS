629ba1e4e2819082267e6db5d17e37ad76222e14
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-05-13 18:00:12 +0530

    trial

--- pages/_error.js
+++ pages/_error.js
@@ -20,8 +20,24 @@ function Error({ statusCode}) {
 }
 
 Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode};
+  // const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  // return { statusCode};
+
+  let statusCode;
+  // If the res variable is defined it means nextjs
+  // is in server side
+  if (res) {
+    statusCode = res.statusCode;
+  } else if (err) {
+    // if there is any error in the app it should
+    // return the status code from here
+    statusCode = err.statusCode;
+  } else {
+    // Something really bad/weird happen and status code
+    // cannot be determined.
+    statusCode = null;
+  }
+  return { statusCode };
 };
 
 export default Error;
