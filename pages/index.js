e15ab69f68108e81243deefd7d80655d608c7465
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-05-13 15:27:59 +0530

    working on reload

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ function Home() {
       {users.map((user) => {
         return (
           <li key={user.id}>
-            <Link href={`/${user.id}`}>
+            <Link href={`/users/${user.id}`}>
               {user.name}
             </Link>
           </li>
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,5 +1,5 @@
-import Error from "./_error";
-import Custom404 from "./404";
+import Error from "../_error";
+import Custom404 from "../404";
 const users = [
   {
     id: 1,
