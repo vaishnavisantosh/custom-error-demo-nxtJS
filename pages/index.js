faf0adeff4c9102523e70c90e18e9752ef8c2e3c
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-05-13 13:39:52 +0530

    generated custome error

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,22 @@ const users = [
   {
     id: 1,
     name: "User1",
-    age: 20
+    age: 20,
   },
   {
     id: 2,
     name: "User2",
-    age: 30
-  }
+    age: 30,
+  },
 ];
 
 function Home() {
   return (
     <ul>
-      {users.map(user => {
+      {user.map((user) => {
         return (
           <li key={user.id}>
-            <Link href={`/users/${users.id}`}>
+            <Link href={`/users/${user.id}`}>
               <a>{user.name}</a>
             </Link>
           </li>
