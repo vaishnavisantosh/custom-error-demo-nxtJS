965c4704bc225b754a29125f4120d90c93917169
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-05-13 15:07:36 +0530

    added reload

--- pages/_error.js
+++ pages/_error.js
@@ -1,11 +1,20 @@
+
+import Router from 'next/router'
 function Error({ statusCode }) {
   return (
+    <>
     <p>
       {statusCode
         ? `An error ${statusCode} occurred on server`
         : "An error occurred on client"}
     </p>
-  );
+     <button type="button" onClick={() => Router.reload()}>
+     Reload 
+   </button>
+   {
+     window.setTimeout(()=>{Router.reload()},5000)
+   }
+  </>);
 }
 
 Error.getInitialProps = ({ res, err }) => {
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ function Home() {
         return (
           <li key={user.id}>
             <Link href={`/users/${user.id}`}>
-              <a>{user.name}</a>
+              {user.name}
             </Link>
           </li>
         );
