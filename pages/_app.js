a3246df82fe18e2daec56a53447e386e82b6e77c
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2020-05-20 18:29:01 +0530

    error-page-layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
 import React from "react";
 import App from "next/app";
 import Error from "./_error";
+import '../styles/public.scss';
 class MyApp extends App {
   constructor(props) {
     super(props);
+  }
+
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    const { res } = ctx;
+
+    const errorCode = res.statusCode > 200 ? res.statusCode : false;
+
+    pageProps.query = ctx.query;
+
+    return {
+      pageProps,
+      errorCode,
+    };
   }
 
   componentDidCatch(error) {
@@ -12,7 +31,7 @@ class MyApp extends App {
 
   render() {
     console.log("inside render---------->");
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, errorCode } = this.props;
 
     return <Component {...pageProps} />;
   }
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,23 @@
 import React, { Component } from "react";
-import Link from "next/link";
-import Error from "./_error";
+import { Grid } from "@material-ui/core";
+// import QLogo from './../src/assets/images/Q-logo.svg';
+// import '../styles/public.scss';
 
-const users = [
-  {
-    id: 1,
-    name: "User1",
-    age: 20,
-  },
-  {
-    id: 2,
-    name: "User2",
-    age: 30,
-  },
-];
-
-const arr = [];
-
-class Home extends Component {
-  componentDidCatch(err) {
-    console.log("errrrrrr0r in didcatch--------->", err);
-  }
-
-  render() {
-    return (
-      <div>
-        <ul>
-          {users.map((user) => {
-            return (
-              <li key={user.id}>
-                <Link href={`/users/${user.id}`}>
-                  <a>{user.name}</a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-        <button onClick={() => arr[0].value}>Custom Error</button>
+export default function AuthLayout(props) {
+  return (<Grid container className="login-page-container">
+    <Grid item sm={6} xs={12} className="login-form">
+      {props.children}
+    </Grid>
+    <Grid item sm={6} xs={0} className="background-img trangles">
+      <div className="trangle-big">
+        <span className="image-1"></span>
+        <span className="shadow-image-1"></span>
+        <span className="image-2"></span>
+        <span className="shadow-image-2"></span>
       </div>
-    );
-  }
+      <div className="trangle-small">
+      </div>
+    </Grid>
+  </Grid>
+  );
 }
-
-export default Home;
